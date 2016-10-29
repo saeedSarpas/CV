a5f4a336097001dff296663286bafcac9eb1182a
Saeed Sarpas <saeed@astro.uni-bonn.de>
2016-10-29 22:09:26 +0200

    Styled header Modified master thesis description

--- auto/preamble.el
+++ auto/preamble.el
@@ -1,25 +1,7 @@
 (TeX-add-style-hook
  "preamble"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("geometry" "a4paper" "left=1cm" "right=1.5cm" "bottom=2.5cm" "top=0cm")))
-   (TeX-run-style-hooks
-    "inputenc"
-    "amsmath"
-    "graphicx"
-    "geometry"
-    "multirow"
-    "array"
-    "pifont"
-    "fontspec")
    (TeX-add-symbols
-    '("parttitle" 1)
-    "name"
-    "addr"
-    "secondlineaddr"
-    "email"
-    "sep"
-    "newpart"
     "newline"))
- :latex)
+ :plain-tex)
 
